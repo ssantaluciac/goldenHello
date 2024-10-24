127b926ee7705550adf2274ad521a98256d72bde
ssantaluciac <stefano_slc@hotmail.com>
2024-10-23 21:37:47 -0400

    boilerPlate

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      // NextJS <Image> component needs to whitelist domains for src={}
+      "lh3.googleusercontent.com",
+      "pbs.twimg.com",
+      "images.unsplash.com",
+      "logos-world.net",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- sections/Different.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Different = () => {
-  return (
-    <div>Different</div>
-  )
-}
-
-export default Different
